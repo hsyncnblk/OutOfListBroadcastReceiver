0fbd5bfeb9623ff88ed30b63abc2562d37d161dd
huseyin cinibulak <huseyincnblk@gmail.com>
2023-12-19 14:07:00 +0300

    Android charging level has been checked

--- app/src/main/java/com/hsyncnblk/sarjseviyekontrol/MainActivity.kt
+++ app/src/main/java/com/hsyncnblk/sarjseviyekontrol/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.hsyncnblk.sarjseviyekontrol
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sarjSeviyesiAlgilama: SarjSeviyesiAlgilama
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // android liste dışı uygulama örneği
+
+        sarjSeviyesiAlgilama= SarjSeviyesiAlgilama()
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter()
+        filter.addAction("android.intent.action.BATTERY_LOW")
+        registerReceiver(sarjSeviyesiAlgilama,filter)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(sarjSeviyesiAlgilama)
+
     }
 }
